0c8cc3ba9c43f721c8b6f86d017c4e71ecd8a358
fulu <946902732@qq.com>
2020-04-17 15:08:41 +0800

    search

--- src/components/alarm/SearchForm.vue
+++ src/components/alarm/SearchForm.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="alarm-summary-search">
     <el-form :inline="true" :model="searchForm" ref="searchForm">
-      <el-form-item label-width>
-        <el-input placeholder disabled v-model="searchContent">
+      <div class="more-search">
+        <!-- <el-input placeholder disabled v-model="searchContent">
           <el-button slot="append" icon="el-icon-caret-bottom" @click="moreSearch = !moreSearch"></el-button>
-        </el-input>
+        </el-input>-->
+        <el-button
+          slot="append"
+          class="more-search-btn"
+          icon="el-icon-caret-bottom"
+          @click="moreSearch = !moreSearch"
+        >全部</el-button>
         <div class="search-box" v-if="moreSearch">
           <el-form-item label="恶意IP">
             <el-input v-model="searchForm.sip" placeholder="恶意IP"></el-input>
@@ -19,7 +25,7 @@
             <el-input v-model="searchForm.attack_type" placeholder="攻击类型"></el-input>
           </el-form-item>
         </div>
-      </el-form-item>
+      </div>
       <el-form-item label="时间">
         <el-date-picker
           :clearable="false"
@@ -88,19 +94,29 @@ export default {
 
 <style  lang="scss">
 .alarm-summary-search {
-  .search-box {
-    position: absolute;
-    background: white;
-    z-index: 10;
-    border: 1px solid #cccccc;
-    border-radius: 4px;
-    width: 100%;
-    padding-top: 18px;
-    overflow: auto;
-    .el-form-item {
-      display: flex;
-      .el-form-item__label {
-        width: 95px;
+  .more-search {
+    display: inline-block;
+    margin-right: 20px;
+    .search-box {
+      position: absolute;
+      background: white;
+      z-index: 10;
+      width: 250px;
+      padding-top: 18px;
+      overflow: auto;
+      width: 220px;
+      box-shadow: 0 0 8px 2px rgba(191, 191, 191, 0.42);
+      border-top-left-radius: 0;
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
+      border-bottom-left-radius: 8px;
+      z-index: 100;
+      .el-form-item {
+        display: flex;
+        .el-form-item__label {
+          width: 95px;
+          font-size: 12px;
+        }
       }
     }
   }
