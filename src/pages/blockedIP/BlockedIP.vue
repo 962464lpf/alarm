f8d9fd57e4c35c2995e63e4be5211c6ca7b6db17
962464lpf <18192051343@163.com>
2020-06-20 15:44:13 +0800

    封禁列表添加导出

--- src/pages/blockedIP/BlockedIP.vue
+++ src/pages/blockedIP/BlockedIP.vue
@@ -1,58 +1,86 @@
 <template>
   <div class="blocked-ip">
-    <el-form :inline="true" :model="form">
+    <el-form :inline="true"
+             :model="form">
       <el-form-item label="IP地址">
-        <el-input v-model="form.ip" placeholder="请输入IP地址"></el-input>
+        <el-input v-model="form.ip"
+                  placeholder="请输入IP地址"></el-input>
+      </el-form-item>
+      <el-form-item label="时间">
+        <el-date-picker v-model="form.time"
+                        type="daterange"
+                        range-separator="至"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        value-format="yyyy-MM-dd">
+        </el-date-picker>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary"
+                   @click="onSearch">查询</el-button>
+        <el-button type="primary"
+                   @click="onReset">重置</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSearch">查询</el-button>
-        <el-button type="primary" @click="onReset">重置</el-button>
+        <el-button type="success"
+                   @click="onAdd">新增</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="success" @click="onAdd">新增</el-button>
+        <el-button type="success"
+                   @click="exportFlie">导出</el-button>
       </el-form-item>
     </el-form>
-    <el-table v-loading="tableLoading" :data="blockedIP" border style="width: 100%">
-      <el-table-column prop="ip" label="IP地址"></el-table-column>
-      <el-table-column prop="created_time" label="封禁时间"></el-table-column>
-      <el-table-column prop="uname" label="封禁人"></el-table-column>
-      <el-table-column label="操作" width="100">
+    <el-table v-loading="tableLoading"
+              :data="blockedIP"
+              border
+              style="width: 100%">
+      <el-table-column prop="ip"
+                       label="IP地址"></el-table-column>
+      <el-table-column prop="created_time"
+                       label="封禁时间"></el-table-column>
+      <el-table-column prop="uname"
+                       label="封禁人"></el-table-column>
+      <el-table-column label="操作"
+                       width="100">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="unBlockedIP(scope.row)">解封</el-button>
+          <el-button type="text"
+                     size="small"
+                     @click="unBlockedIP(scope.row)">解封</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-      class="fr mt10"
-      background
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage"
-      :page-sizes="[10, 20, 30, 40]"
-      :page-size="pageSize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-    ></el-pagination>
+    <el-pagination class="fr mt10"
+                   background
+                   @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange"
+                   :current-page="currentPage"
+                   :page-sizes="[10, 20, 30, 40]"
+                   :page-size="pageSize"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="total"></el-pagination>
     <div class="clearfloat"></div>
 
     <div v-if="addBlockedIPStatus">
-      <addBlockedIP v-model="addBlockedIPStatus" @getAddBlockedIP="getAddBlockedIP"></addBlockedIP>
+      <addBlockedIP v-model="addBlockedIPStatus"
+                    @getAddBlockedIP="getAddBlockedIP"></addBlockedIP>
     </div>
   </div>
 </template>
 
 <script>
-import { getBlockedIApi, unBlockedIPApi, aKeyBlockedApi } from '../../tools/api'
+import { getBlockedIApi, unBlockedIPApi, aKeyBlockedApi, downloadFileApi } from '../../tools/api'
 import addBlockedIP from '../../components/blockedIP/AddBlockedIP'
 import { mapState } from 'vuex'
 export default {
   components: {
     addBlockedIP
   },
-  data() {
+  data () {
     return {
       form: {
-        ip: ''
+        ip: '',
+        time: []
       },
       tableLoading: false,
       blockedIP: [],
@@ -66,17 +94,30 @@ export default {
     ...mapState(['userInfo'])
   },
   methods: {
-    onSearch() {
+    exportFlie () {
+      let url = ''
+      let type = ''
+      downloadFileApi(url).then(res => {
+        let blob = new Blob([res], { type })
+        let url = window.URL.createObjectURL(blob)
+        let a = document.createElement('a')
+        a.setAttribute('download', 'text')
+        a.setAttribute('href', url)
+        a.click()
+      })
+    },
+    onSearch () {
+      console.log(this.form.time)
       this.getBlockedIP()
     },
-    onReset() {
+    onReset () {
       this.form.ip = ''
       this.getBlockedIP()
     },
-    onAdd() {
+    onAdd () {
       this.addBlockedIPStatus = true
     },
-    getAddBlockedIP(form) {
+    getAddBlockedIP (form) {
       let fd = new FormData()
       fd.append('ip', form.ip)
       fd.append('id', this.userInfo.id)
@@ -93,7 +134,7 @@ export default {
         })
       })
     },
-    unBlockedIP(row) {
+    unBlockedIP (row) {
       if (this.userInfo.level === 0) {
         this.$confirm('您确定要将此IP进行解封吗?', '提示', {
           confirmButtonText: '确定',
@@ -122,15 +163,15 @@ export default {
         })
       }
     },
-    handleSizeChange(val) {
+    handleSizeChange (val) {
       this.pageSize = val
       this.getBlockedIP()
     },
-    handleCurrentChange(val) {
+    handleCurrentChange (val) {
       this.currentPage = val
       this.getBlockedIP()
     },
-    getBlockedIP() {
+    getBlockedIP () {
       this.tableLoading = true
       let fd = new FormData()
       fd.append('page', this.currentPage)
@@ -143,7 +184,7 @@ export default {
       })
     }
   },
-  mounted() {
+  mounted () {
     this.getBlockedIP()
   }
 }
