77b450c10169ee083741adb3e2ab292c3c9fef80
962464lpf <18192051343@163.com>
2020-06-21 11:37:49 +0800

    报告管理添加创建时间

--- src/pages/report/Report.vue
+++ src/pages/report/Report.vue
@@ -1,50 +1,64 @@
 <template>
   <div class="report">
     <el-row>
-      <el-form :inline="true" :model="searchForm" class="demo-form-inline">
+      <el-form :inline="true"
+               :model="searchForm"
+               class="demo-form-inline">
         <el-form-item label="统计日期">
-          <el-date-picker
-            v-model="searchForm.date"
-            type="date"
-            range-separator="至"
-            value-format="yyyy-MM-dd"
-          ></el-date-picker>
+          <el-date-picker v-model="searchForm.date"
+                          type="date"
+                          range-separator="至"
+                          value-format="yyyy-MM-dd"></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button @click="getReportList" type="primary">查询</el-button>
+          <el-button @click="getReportList"
+                     type="primary">查询</el-button>
           <el-button @click="resetData">重置</el-button>
         </el-form-item>
       </el-form>
     </el-row>
     <div class="clearfloat"></div>
-    <el-table class="mt10" v-loading="tableLoading" :data="reportData" style="width: 100%" border>
-      <el-table-column prop="name" label="报告名称"></el-table-column>
-      <el-table-column prop="url" label="报告地址">
+    <el-table class="mt10"
+              v-loading="tableLoading"
+              :data="reportData"
+              style="width: 100%"
+              border>
+      <el-table-column prop="name"
+                       label="报告名称"></el-table-column>
+      <el-table-column prop="url"
+                       label="报告地址">
         <template slot-scope="scope">
-          <el-tooltip class="item" effect="dark" :content="scope.row.url" placement="bottom">
+          <el-tooltip class="item"
+                      effect="dark"
+                      :content="scope.row.url"
+                      placement="bottom">
             <span class="curp omit">{{ scope.row.url}}</span>
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column prop="type" label="报告类型"></el-table-column>
-      <el-table-column prop="tongji_riqi" label="统计日期"></el-table-column>
-      <el-table-column label="操作" width="50">
+      <el-table-column prop="type"
+                       label="报告类型"></el-table-column>
+      <el-table-column prop="tongji_riqi"
+                       label="统计日期"></el-table-column>
+      <el-table-column prop="created_time"
+                       label="创建时间"></el-table-column>
+      <el-table-column label="操作"
+                       width="50">
         <template slot-scope="scope">
-          <el-button type="text" @click.native="downloadReport(scope.row)">下载</el-button>
+          <el-button type="text"
+                     @click.native="downloadReport(scope.row)">下载</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-      class="fr mt10"
-      background
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage"
-      :page-sizes="[10, 20, 30, 40]"
-      :page-size="pageSize"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total"
-    ></el-pagination>
+    <el-pagination class="fr mt10"
+                   background
+                   @size-change="handleSizeChange"
+                   @current-change="handleCurrentChange"
+                   :current-page="currentPage"
+                   :page-sizes="[10, 20, 30, 40]"
+                   :page-size="pageSize"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="total"></el-pagination>
     <div class="clearfloat"></div>
   </div>
 </template>
@@ -52,7 +66,7 @@
 <script>
 import { getReportListApi, downloadFileApi, BASE_URL } from '../../tools/api'
 export default {
-  data() {
+  data () {
     return {
       searchForm: {
         date: ''
@@ -66,11 +80,11 @@ export default {
     }
   },
   methods: {
-    resetData() {
+    resetData () {
       this.searchForm.date = ''
       this.getReportList()
     },
-    downloadReport(row) {
+    downloadReport (row) {
       let length = row.url.split('.').length
       let type = row.url.split('.')[length - 1]
       let name = row.url.split('.')[0].split('/')[
@@ -91,15 +105,15 @@ export default {
         a.click()
       })
     },
-    handleSizeChange(val) {
+    handleSizeChange (val) {
       this.pageSize = val
       this.getReportList()
     },
-    handleCurrentChange(val) {
+    handleCurrentChange (val) {
       this.currentPage = val
       this.getReportList()
     },
-    getReportList() {
+    getReportList () {
       let fd = new FormData()
       // page,  per_page
       fd.append('page', this.currentPage)
@@ -112,7 +126,7 @@ export default {
       })
     }
   },
-  mounted() {
+  mounted () {
     this.getReportList()
   }
 }
