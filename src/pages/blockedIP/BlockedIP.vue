a053efd703398599d7bd60fd4b8297220d67d4b2
fulu <946902732@qq.com>
2020-08-13 15:32:54 +0800

    时间参数部位undef

--- src/pages/blockedIP/BlockedIP.vue
+++ src/pages/blockedIP/BlockedIP.vue
@@ -1,82 +1,66 @@
 <template>
   <div class="blocked-ip">
-    <el-form :inline="true"
-             :model="form">
+    <el-form :inline="true" :model="form">
       <el-form-item label="IP地址">
-        <el-input v-model="form.ip"
-                  placeholder="请输入IP地址"></el-input>
+        <el-input v-model="form.ip" placeholder="请输入IP地址"></el-input>
       </el-form-item>
       <el-form-item label="时间">
-        <el-date-picker v-model="form.time"
-                        type="datetimerange"
-                        range-separator="至"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期"
-                        value-format="yyyy-MM-dd HH-mm-ss"></el-date-picker>
+        <el-date-picker
+          v-model="form.time"
+          type="datetimerange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd HH-mm-ss"
+        ></el-date-picker>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary"
-                   @click="onSearch">查询</el-button>
-        <el-button type="primary"
-                   @click="onReset">重置</el-button>
+        <el-button type="primary" @click="onSearch">查询</el-button>
+        <el-button type="primary" @click="onReset">重置</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary"
-                   @click="onAdd">新增</el-button>
+        <el-button type="primary" @click="onAdd">新增</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary"
-                   @click="exportFlie">导出</el-button>
+        <el-button type="primary" @click="exportFlie">导出</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary"
-                   @click="batchBlockedIPStatus = true">批量封禁</el-button>
+        <el-button type="primary" @click="batchBlockedIPStatus = true">批量封禁</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary"
-                   @click="downloadBlockedFile">下载封禁模板</el-button>
+        <el-button type="primary" @click="downloadBlockedFile">下载封禁模板</el-button>
       </el-form-item>
     </el-form>
-    <el-table v-loading="tableLoading"
-              :data="blockedIP"
-              border
-              style="width: 100%">
-      <el-table-column prop="ip"
-                       label="IP地址"></el-table-column>
-      <el-table-column prop="fname"
-                       label="封禁防火墙"></el-table-column>
-      <el-table-column prop="created_time"
-                       label="封禁时间"></el-table-column>
-      <el-table-column prop="uname"
-                       label="封禁人"></el-table-column>
-      <el-table-column label="操作"
-                       width="100">
+    <el-table v-loading="tableLoading" :data="blockedIP" border style="width: 100%">
+      <el-table-column prop="ip" label="IP地址"></el-table-column>
+      <el-table-column prop="fname" label="封禁防火墙"></el-table-column>
+      <el-table-column prop="created_time" label="封禁时间"></el-table-column>
+      <el-table-column prop="uname" label="封禁人"></el-table-column>
+      <el-table-column label="操作" width="100">
         <template slot-scope="scope">
-          <el-button type="text"
-                     size="small"
-                     @click="unBlockedIP(scope.row)">解封</el-button>
+          <el-button type="text" size="small" @click="unBlockedIP(scope.row)">解封</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination class="fr mt10"
-                   background
-                   @size-change="handleSizeChange"
-                   @current-change="handleCurrentChange"
-                   :current-page="currentPage"
-                   :page-sizes="[10, 20, 30, 40]"
-                   :page-size="pageSize"
-                   layout="total, sizes, prev, pager, next, jumper"
-                   :total="total"></el-pagination>
+    <el-pagination
+      class="fr mt10"
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
     <div class="clearfloat"></div>
 
     <div v-if="addBlockedIPStatus">
-      <addBlockedIP v-model="addBlockedIPStatus"
-                    @getAddBlockedIP="getAddBlockedIP"></addBlockedIP>
+      <addBlockedIP v-model="addBlockedIPStatus" @getAddBlockedIP="getAddBlockedIP"></addBlockedIP>
     </div>
     <div v-if="batchBlockedIPStatus">
-      <batchBlockedIP v-model="batchBlockedIPStatus"
-                      @updateTableData="getBlockedIP"></batchBlockedIP>
+      <batchBlockedIP v-model="batchBlockedIPStatus" @updateTableData="getBlockedIP"></batchBlockedIP>
     </div>
   </div>
 </template>
@@ -99,7 +83,7 @@ export default {
     addBlockedIP,
     batchBlockedIP
   },
-  data () {
+  data() {
     return {
       form: {
         ip: '',
@@ -118,10 +102,10 @@ export default {
     ...mapState(['userInfo'])
   },
   methods: {
-    exportFlie () {
+    exportFlie() {
       let fd = new FormData()
-      fd.append('start_time', this.form.time[0])
-      fd.append('end_time', this.form.time[1])
+      fd.append('start_time', this.form.time[0] ? this.form.time[0] : '')
+      fd.append('end_time', this.form.time[1] ? this.form.time[1] : '')
       fd.append('ip', this.form.ip)
       downloadBlockedIPApi(fd).then(res => {
         let url = BASE_URL + res.file_path
@@ -136,24 +120,24 @@ export default {
         })
       })
     },
-    onSearch () {
+    onSearch() {
       this.getBlockedIP()
     },
-    onReset () {
+    onReset() {
       this.form.ip = ''
       this.form.time = ['', '']
       this.getBlockedIP()
     },
-    onAdd () {
+    onAdd() {
       this.addBlockedIPStatus = true
     },
-    downloadBlockedFile () {
+    downloadBlockedFile() {
       let a = document.createElement('a')
       a.setAttribute('download', '模板')
       a.setAttribute('href', BASE_URL + '/forbidden_moban.xlsx')
       a.click()
     },
-    getAddBlockedIP (form) {
+    getAddBlockedIP(form) {
       const loading = this.$loading({
         lock: true,
         text: 'Loading',
@@ -177,7 +161,7 @@ export default {
         })
       })
     },
-    unBlockedIP (row) {
+    unBlockedIP(row) {
       if (this.userInfo.level === 0) {
         this.$confirm('您确定要将此IP进行解封吗?', '提示', {
           confirmButtonText: '确定',
@@ -206,15 +190,15 @@ export default {
         })
       }
     },
-    handleSizeChange (val) {
+    handleSizeChange(val) {
       this.pageSize = val
       this.getBlockedIP()
     },
-    handleCurrentChange (val) {
+    handleCurrentChange(val) {
       this.currentPage = val
       this.getBlockedIP()
     },
-    getBlockedIP () {
+    getBlockedIP() {
       this.tableLoading = true
       let fd = new FormData()
       fd.append('start_time', this.form.time[0])
@@ -229,7 +213,7 @@ export default {
       })
     }
   },
-  mounted () {
+  mounted() {
     this.getBlockedIP()
   }
 }
