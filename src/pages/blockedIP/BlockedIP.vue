8518fd3940a7d16eb944764f5e506b646dfab734
fulu <946902732@qq.com>
2020-08-14 09:43:01 +0800

    update

--- src/pages/blockedIP/BlockedIP.vue
+++ src/pages/blockedIP/BlockedIP.vue
@@ -109,7 +109,8 @@ export default {
       fd.append('ip', this.form.ip)
       this.$message({
         type: 'warning',
-        message: '导出文件可能需要的时间较长，请等待！'
+        message: '导出文件可能需要的时间较长，请等待！',
+        duration: 1500
       })
       downloadBlockedIPApi(fd)
         .then(res => {
