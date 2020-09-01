e58649ac85061a9949dc41844eb1f212e169b23f
fulu <946902732@qq.com>
2020-09-01 10:25:23 +0800

    浮点类型相减有多位小数

--- src/pages/sys/EquipStatus.vue
+++ src/pages/sys/EquipStatus.vue
@@ -4,35 +4,28 @@
       <div class="title">磁盘状态</div>
       <div class="pl25">
         <el-progress :percentage="diskStatus"></el-progress>
-        <p>已使用{{equipStatus.totalDisk - equipStatus.freeDisk}}GB 可用{{equipStatus.freeDisk}} GB 共{{equipStatus.totalDisk}}GB</p>
+        <p>已使用{{((equipStatus.totalDisk - equipStatus.freeDisk) * m) / m}}GB 可用{{equipStatus.freeDisk}} GB 共{{equipStatus.totalDisk}}GB</p>
       </div>
     </el-row>
     <el-row>
       <div class="title">内存状态</div>
       <div class="pl25">
         <el-progress :percentage="memoryStatus"></el-progress>
-        <p>已使用{{equipStatus.totalMemory - equipStatus.freeMemory}}GB 可用{{equipStatus.freeMemory}} GB 共{{equipStatus.totalMemory}}GB</p>
+        <p>已使用{{((equipStatus.totalMemory- equipStatus.freeMemory) * m) / m}}GB 可用{{equipStatus.freeMemory}} GB 共{{equipStatus.totalMemory}}GB</p>
       </div>
     </el-row>
-    <!-- <el-row class="mt10">
-      <div class="title">负载状态</div>
-      <el-row class="pl25">
-        <el-col :span="12">
-          <ve-line :data="cpuChartData" :settings="cpuChartSettings"></ve-line>
-        </el-col>
-        <el-col :span="12">
-          <ve-line :data="memoryChartData" :settings="memoryChartSettings"></ve-line>
-        </el-col>
-      </el-row>
-    </el-row>-->
+    <el-row>
+      <div class="title">CPU状态</div>
+      <div class="pl25">
+        <el-progress :percentage="cpuStatus"></el-progress>
+      </div>
+    </el-row>
     <el-row class="mt10">
       <div class="title">系统时间</div>
-      <!-- <div class="pl25">系统时间：2020-8-31 14:16:23</div> -->
       <div class="pl25">系统运行时间：{{equipStatus.runTime}} 天</div>
     </el-row>
     <el-row class="mt10">
       <div class="title">内核版本</div>
-      <!-- <div class="pl25">系统时间：2020-8-31 14:16:23</div> -->
       <div class="pl25">{{equipStatus.modelName}} 天</div>
     </el-row>
   </div>
@@ -46,76 +39,39 @@ export default {
       equipStatus: {
         totalDisk: 0,
         freeDisk: 0,
-        totalMemory: '',
-        freeMemory: '',
+        totalMemory: 0,
+        freeMemory: 0,
         runTime: '',
         modelName: '',
       },
-      cpuChartData: {
-        columns: ['日期', 'cpu使用率'],
-        rows: [
-          { 日期: '11:30', cpu使用率: 0.45 },
-          { 日期: '12:00', cpu使用率: 0.65 },
-          { 日期: '12:30', cpu使用率: 0.22 },
-          { 日期: '13:00', cpu使用率: 0.36 },
-          { 日期: '13:30', cpu使用率: 0.48 },
-          { 日期: '14:00', cpu使用率: 0.36 },
-        ],
-      },
-      cpuChartSettings: {
-        yAxisType: ['percent'],
-      },
-      memoryChartData: {
-        columns: ['日期', '内存使用率'],
-        rows: [
-          { 日期: '11:30', 内存使用率: 0.45 },
-          { 日期: '12:00', 内存使用率: 0.65 },
-          { 日期: '12:30', 内存使用率: 0.22 },
-          { 日期: '13:00', 内存使用率: 0.36 },
-          { 日期: '13:30', 内存使用率: 0.48 },
-          { 日期: '14:00', 内存使用率: 0.36 },
-        ],
-      },
-      memoryChartSettings: {
-        yAxisType: ['percent'],
-      },
+      cpuStatus: 0,
+      m: Math.pow(10, 20),
     }
   },
   computed: {
     diskStatus() {
-      return (
-        (
-          (this.equipStatus.totalDisk - this.equipStatus.freeDisk) /
-          this.equipStatus.totalDisk
-        ).toFixed(3) * 100
-      )
+      let use =
+        ((this.equipStatus.totalDisk - this.equipStatus.freeDisk) * this.m) /
+        this.m
+      return (use / this.equipStatus.totalDisk).toFixed(3) * 100
     },
     memoryStatus() {
-      return (
-        (
-          (this.equipStatus.totalMemory - this.equipStatus.freeMemory) /
-          this.equipStatus.freeMemory
-        ).toFixed(3) * 100
-      )
+      let use =
+        ((this.equipStatus.totalMemory - this.equipStatus.freeMemory) *
+          this.m) /
+        this.m
+      return (use / this.equipStatus.freeMemory).toFixed(3) * 100
     },
   },
   methods: {},
   mounted() {
     getHardWareApi().then((res) => {
       this.equipStatus.totalDisk = res.total_disk
-      this.equipStatus.freeDisk = res.free_disk.slice(
-        0,
-        res.free_disk.length - 1
-      )
-      this.equipStatus.totalMemory = res.total_mem.slice(
-        0,
-        res.total_mem.length - 1
-      )
-      this.equipStatus.freeMemory = res.free_mem.slice(
-        0,
-        res.free_mem.length - 1
-      )
-      this.equipStatus.runTime = res.run_time
+      this.equipStatus.freeDisk = parseFloat(res.free_disk)
+      this.equipStatus.totalMemory = parseFloat(res.total_mem)
+      this.equipStatus.freeMemory = parseFloat(res.free_mem)
+      this.cpuStatus = this.equipStatus.runTime = res.run_time
+      this.cpuStatus = parseFloat(res.cpu_use)
       this.equipStatus.modelName = res.model_name
     })
   },
