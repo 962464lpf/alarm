e009edeaa1d17d76c49477b1e66d621c0a541cb4
fulu <946902732@qq.com>
2020-11-04 14:48:45 +0800

    资产管理

--- src/pages/sys/Inside.vue
+++ src/pages/sys/Inside.vue
@@ -1,102 +1,101 @@
 <template>
   <div class="inside">
-    <el-form :inline="true"
-             :model="form"
-             class="demo-form-inline">
+    <el-form :inline="true" :model="form" class="demo-form-inline my-elem-form">
       <el-form-item label="公网IP">
-        <el-input v-model="form.ip"></el-input>
+        <el-input v-model="form.ip">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </el-form-item>
       <el-form-item label="私网IP">
-        <el-input v-model="form.ip_private"></el-input>
+        <el-input v-model="form.ip_private">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </el-form-item>
       <el-form-item label="资产名称">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="form.name">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </el-form-item>
       <el-form-item label="责任人">
-        <el-input v-model="form.staff"></el-input>
+        <el-input v-model="form.staff">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
       </el-form-item>
       <el-form-item label="是否在线">
-        <el-switch v-model="form.online"
-                   active-text="在线"
-                   inactive-text="离线">
-        </el-switch>
+        <el-switch
+          v-model="form.online"
+          class="my-switch"
+          active-text="在线"
+          inactive-text="离线"
+          active-color="#95d214"
+        ></el-switch>
       </el-form-item>
       <el-form-item label="服务器区">
-        <el-switch v-model="form.is_server" active-text="是" inactive-text="否"></el-switch>
+        <el-switch
+          class="my-switch"
+          v-model="form.is_server"
+          active-text="是"
+          inactive-text="否"
+          active-color="#95d214"
+        ></el-switch>
       </el-form-item>
       <el-form-item>
-        <el-button @click="getInsideEquip"
-                   type="primary"
-                   size="small">查询</el-button>
-        <el-button @click="reset"
-                   type="primary"
-                   size="small">重置</el-button>
-        <el-button @click="entryInsideEquip('single')"
-                   type="primary">资产录入</el-button>
-        <el-button @click="entryInsideEquip('more')"
-                   type="primary">批量录入</el-button>
-        <el-button type="primary"
-                   @click="downloadInsideFile">下载录入模板</el-button>
+        <el-button @click="getInsideEquip" type="primary" size="small" class="my-elem-btn">查询</el-button>
+        <el-button @click="reset" type="primary" size="small" class="my-elem-btn">重置</el-button>
+        <el-button @click="entryInsideEquip('single')" type="primary" class="my-elem-btn">资产录入</el-button>
+        <el-button @click="entryInsideEquip('more')" type="primary" class="my-elem-btn">批量录入</el-button>
+        <el-button type="primary" @click="downloadInsideFile" class="my-elem-btn">下载录入模板</el-button>
       </el-form-item>
     </el-form>
-    <el-table v-loading="tableLoading"
-              :data="insideEquipData"
-              stripe
-              border
-              style="width: 100%"
-              class="mt10">
-      <el-table-column prop="name"
-                       label="资产名称"></el-table-column>
-      <el-table-column prop="ip"
-                       label="公网IP"></el-table-column>
-      <el-table-column prop="ip_private"
-                       label="私网IP"></el-table-column>
-      <el-table-column prop="anquanyu"
-                       label="安全域"></el-table-column>
-      <el-table-column prop="com_dep"
-                       label="单位-部门"></el-table-column>
-      <el-table-column prop="cat"
-                       label="类型"></el-table-column>
-      <el-table-column prop="staff"
-                       label="责任人"></el-table-column>
-      <el-table-column prop="phone"
-                       label="联系电话"></el-table-column>
+    <el-table
+      v-loading="tableLoading"
+      :data="insideEquipData"
+      stripe
+      border
+      style="width: 100%"
+      class="mt10"
+    >
+      <el-table-column prop="name" label="资产名称"></el-table-column>
+      <el-table-column prop="ip" label="公网IP"></el-table-column>
+      <el-table-column prop="ip_private" label="私网IP"></el-table-column>
+      <el-table-column prop="anquanyu" label="安全域"></el-table-column>
+      <el-table-column prop="com_dep" label="单位-部门"></el-table-column>
+      <el-table-column prop="cat" label="类型"></el-table-column>
+      <el-table-column prop="staff" label="责任人"></el-table-column>
+      <el-table-column prop="phone" label="联系电话"></el-table-column>
       <el-table-column prop="is_server" label="服务器区">
         <template slot-scope="scope">{{scope.row.is_server === 0 ? '否' : '是'}}</template>
-      </el-table-column>    
-      <el-table-column label="是否在线" width="180">
-        <template slot-scope="scope">
-          {{scope.row.online ? '在线' : '离线'}}
-        </template>
       </el-table-column>
-      <el-table-column label="操作"
-                       width="180">
+      <el-table-column label="是否在线" width="180">
+        <template slot-scope="scope">{{scope.row.online ? '在线' : '离线'}}</template>
+      </el-table-column>
+      <el-table-column label="操作" width="180">
         <template slot-scope="scope">
-          <el-button @click="deleteEquip(scope.row)"
-                     type="text"
-                     size="small">删除</el-button>
-          <el-button @click="modify(scope.row)"
-                     type="text"
-                     size="small">修改</el-button>
+          <el-button @click="deleteEquip(scope.row)" type="text" size="small">删除</el-button>
+          <el-button @click="modify(scope.row)" type="text" size="small">修改</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination class="fr mt10"
-                   background
-                   @size-change="handleSizeChange"
-                   @current-change="handleCurrentChange"
-                   :current-page="currentPage"
-                   :page-sizes="[10, 20, 30, 40]"
-                   :page-size="pageSize"
-                   layout="total, sizes, prev, pager, next, jumper"
-                   :total="total"></el-pagination>
+    <el-pagination
+      class="fr mt10"
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+    ></el-pagination>
     <div class="clearfloat"></div>
     <div v-if="addInsideEquipStatus">
-      <AddInsideEquip v-model="addInsideEquipStatus"
-                      :addInsideEquipType="addInsideEquipType"
-                      :currentRow="currentRow"
-                      @postRequest="postRequest"
-                      @showNotify="showNotify"></AddInsideEquip>
+      <AddInsideEquip
+        v-model="addInsideEquipStatus"
+        :addInsideEquipType="addInsideEquipType"
+        :currentRow="currentRow"
+        @postRequest="postRequest"
+        @showNotify="showNotify"
+      ></AddInsideEquip>
     </div>
   </div>
 </template>
@@ -106,10 +105,10 @@ import AddInsideEquip from '../../components/sys/AddInsideEquip'
 import {
   getInsideEquipApi,
   deleteInsideEquipApi,
-  BASE_URL
+  BASE_URL,
 } from '../../tools/api'
 export default {
-  data () {
+  data() {
     return {
       form: {
         ip: '',
@@ -117,7 +116,7 @@ export default {
         staff: '',
         ip_private: '',
         online: true,
-        is_server: false
+        is_server: false,
       },
       pageSize: 20,
       total: 0,
@@ -127,42 +126,42 @@ export default {
       addInsideEquipStatus: false,
       addInsideEquipType: '',
       currentRow: null,
-      notify: null
+      notify: null,
     }
   },
   components: {
-    AddInsideEquip
+    AddInsideEquip,
   },
   methods: {
-    showNotify () {
+    showNotify() {
       this.duration = 0
       this.notify = this.$notify({
         title: '提示',
         message: '资产正在录入...',
         type: 'info',
         duration: 0,
-        position: 'bottom-right'
+        position: 'bottom-right',
       })
     },
-    downloadInsideFile () {
+    downloadInsideFile() {
       let a = document.createElement('a')
       a.setAttribute('download', '模板')
       a.setAttribute('href', BASE_URL + '/zichan.xlsx')
       a.click()
     },
-    entryInsideEquip (type) {
+    entryInsideEquip(type) {
       this.addInsideEquipStatus = true
       this.addInsideEquipType = type
     },
-    deleteEquip (row) {
+    deleteEquip(row) {
       this.$confirm('您确定要删除此数据吗?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
-        type: 'warning'
+        type: 'warning',
       }).then(() => {
         let fd = new FormData()
         fd.append('id', row.id)
-        deleteInsideEquipApi(fd).then(res => {
+        deleteInsideEquipApi(fd).then((res) => {
           let type = 'success'
           if (res.state !== this.successFlag) {
             type = 'warning'
@@ -171,32 +170,32 @@ export default {
           }
           this.$message({
             type,
-            message: res.info
+            message: res.info,
           })
         })
       })
     },
-    modify (row) {
+    modify(row) {
       this.currentRow = row
       this.addInsideEquipStatus = true
       this.addInsideEquipType = 'modify'
     },
-    handleSizeChange (val) {
+    handleSizeChange(val) {
       this.pageSize = val
       this.getInsideEquip()
     },
-    handleCurrentChange (val) {
+    handleCurrentChange(val) {
       this.currentPage = val
       this.getInsideEquip()
     },
-    reset () {
+    reset() {
       for (let key in this.form) {
         this.form[key] = ''
       }
       this.getInsideEquip()
     },
 
-    getInsideEquip () {
+    getInsideEquip() {
       this.tableLoading = false
       let fd = new FormData()
       for (let key in this.form) {
@@ -208,15 +207,15 @@ export default {
       }
       fd.append('page', this.currentPage)
       fd.append('per_page', this.pageSize)
-      getInsideEquipApi(fd).then(res => {
+      getInsideEquipApi(fd).then((res) => {
         this.tableLoading = false
         this.insideEquipData = res.data
         this.total = res.total
       })
     },
 
-    postRequest ({ api, fd }) {
-      api(fd).then(res => {
+    postRequest({ api, fd }) {
+      api(fd).then((res) => {
         if (this.notify) this.notify.close()
         let type = 'success'
         if (res.state !== this.successFlag) {
@@ -226,18 +225,26 @@ export default {
         }
         this.$message({
           type,
-          message: res.info
+          message: res.info,
         })
       })
-    }
+    },
   },
-  mounted () {
+  mounted() {
     // this.getInsideEquip()
-  }
+  },
 }
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss">
 .inside {
+  .my-switch {
+    .el-switch__label {
+      color: #8e8a8a !important;
+    }
+    .is-active {
+      color: #95d214 !important;
+    }
+  }
 }
 </style>
